c6859e933766cc5fcc13ac74f715c058f9242da7
anjalimandal398 <anjalikumarimandaljms@gmail.com>
2025-08-15 13:44:57 +0530

    binary number

---
+++ ImpQuestion/fabonacci.cpp
@@ -0,0 +1,16 @@
+//fibonacci 0112358
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a=0,b=1, next;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+   cout<<"fibonacci series is: " ;
+    for(int i =1;i<n;i++){
+        cout << a << " ";
+        next=a+b;
+        a=b;
+        b=next;
+    }
+    cout<<next;
+}
